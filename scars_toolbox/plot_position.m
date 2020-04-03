8289e7e4576df2e0e25018f4b0a92f7b0facb206
Adam Śmiałek <smialekadam@gmail.com>
2020-04-03 11:07:42 +0200

    Next WIP

--- scars_toolbox/plot_position.m
+++ scars_toolbox/plot_position.m
@@ -1,14 +1,38 @@
-TStart = 0;
-TFinal = 100000;
-mdlWks = get_param('scars_model','ModelWorkspace');
-reload(mdlWks)
-scars_out = sim('scars_model' ,[TStart TFinal]);
+close all
 
+%% loading
+TStart = 0;
+TFinal = 2000;
+dT = 0.2;
+t = TStart:dT:TFinal;
+
+%% simulation
+disp('scars')
+warning('off','all')
+scars_out = sim('scars_model' ,[TStart TFinal]);
+warning('on','all')
+disp('cube')
+cube = sim('cubesat_propagation' ,[TStart TFinal]);
+disp('done')
+
+%% plotting
+close all
+
+figure(1)
 [x, y, z] = sphere;
 surf(x*Re,y*Re,z*Re)
 axis equal
 hold on
-plot3(scars_out.pos(:,1),scars_out.pos(:,2),scars_out.pos(:,3),'r','LineWidth',3)
-plot3(scars_out.pos(end,1),scars_out.pos(end,2),scars_out.pos(end,3), 'o')
-plot3(scars_out.pos1(:,1),scars_out.pos1(:,2),scars_out.pos1(:,3),'g','LineWidth',3)
-plot3(scars_out.pos1(end,1),scars_out.pos1(end,2),scars_out.pos1(end,3), 'o')
+plot3(scars_out.r_ecef(1,1),scars_out.r_ecef(1,2),scars_out.r_ecef(1,3), 'rx', 'LineWidth',3)
+plot3(scars_out.r_ecef(:,1),scars_out.r_ecef(:,2),scars_out.r_ecef(:,3),'r','LineWidth',3)
+plot3(scars_out.r_ecef(end,1),scars_out.r_ecef(end,2),scars_out.r_ecef(end,3), 'ro', 'LineWidth',3)
+
+plot3(cube.r_ecef(1,1),cube.r_ecef(1,2),cube.r_ecef(1,3), 'gx', 'LineWidth',3)
+plot3(cube.r_ecef(:,1),cube.r_ecef(:,2),cube.r_ecef(:,3),'g','LineWidth',3)
+plot3(cube.r_ecef(end,1),cube.r_ecef(end,2),cube.r_ecef(end,3), 'go', 'LineWidth',3)
+
+figure(2)
+subplot(1,2,1)
+plot(t,scars_out.v_ecef(:,1),t,scars_out.v_ecef(:,2),t,scars_out.v_ecef(:,3))
+subplot(1,2,2)
+plot(t,cube.v_ecef(:,1),t,cube.v_ecef(:,2),t,cube.v_ecef(:,3))
--- scars_toolbox/startup.m
+++ scars_toolbox/startup.m
@@ -3,18 +3,18 @@ G = 6.6743015e-11;
 Re = 6356e3;
 Me = 5.972e24;
 
+omega = 7.2921150e-5;
+
 vy = sqrt(G*Me/h);
 v0 = [0 vy 0];
 
 
-a = 7000e3;
-e = 0.01;
+a = 8000e3;
+e = 0.0;
 incl = deg2rad(45);
-RA = deg2rad(10);
-w = deg2rad(10);
-TA = deg2rad(10);
+RA = deg2rad(90);
+w = deg2rad(0);
+TA = deg2rad(0);
 mu = 3.986004418e14;
 
-[r, v] = kep2rv(a, e, RA, incl, w, TA, mu);
-r_eci = [r;initCond.r_eci]
-v_eci = [v;initCond.v_eci]
+dT = 1;
