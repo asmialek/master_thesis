b0dfc85bbfb7bb8cc932b7bd35d38af7fb67859f
Adam Śmiałek <smialekadam@gmail.com>
2020-07-31 22:28:33 +0200

    Local changes

--- scars_toolbox/plot_example.m
+++ scars_toolbox/plot_example.m
@@ -4,14 +4,41 @@ t = scars_out.tout;
 plot_error = 60*rad2deg(scars_out.error.Data);
 
 figure()
-% subplot(311)
-% plot(t, plot_error(:,1))
-% subplot(312)
-% plot(t, plot_error(:,2))
-% subplot(313)
-% plot(t, plot_error(:,3))
 plot(t, plot_error(:,1), t, plot_error(:,2), t, plot_error(:,3))
-ylim([-600 600])
-% xlim([250 400])
-ylabel('Euler Angles [arcmin]')
+hold on
+plot([220 220], [-1000, 1000], 'k--')
+plot([230 230], [-1000, 1000], 'k--')
+ylim([-400 400])
+% xlim([200 250])
+ylabel('Euler angles error [arcmin]')
 xlabel('Time [s]')
+legend('Roll angle', 'Pitch angle', 'Yaw angle', 'Exposition time');
+set(gcf, 'Position', [100, 100, 900, 500])
+
+figure()
+plot(t, plot_error(:,1), t, plot_error(:,2), t, plot_error(:,3))
+hold on
+plot([220 220], [-1000, 1000], 'k--')
+plot([-1000 1000], [10, 10], 'g--')
+plot([230 230], [-1000, 1000], 'k--')
+plot([-1000 1000], [-10, -10], 'g--')
+ylim([-25 25])
+xlim([200 250])
+ylabel('Euler angles error [arcmin]')
+xlabel('Time [s]')
+legend('Roll angle', 'Pitch angle', 'Yaw angle', 'Exposition time', 'Accuracy requirement');
+set(gcf, 'Position', [100, 100, 900, 500])
+
+figure()
+plot(t, plot_lla(:,1)-1, t, plot_lla(:,2))
+ylabel('Geographical coordinates [deg]')
+xlabel('Time [s]')
+legend('Latitude', 'Longitude')
+set(gcf, 'Position', [100, 100, 400, 400])
+
+figure()
+plot(t, scars_out.ref.Data(:,1), t, scars_out.ref.Data(:,2), t, scars_out.ref.Data(:,3))
+legend('Roll angle', 'Pitch angle', 'Yaw angle')
+ylabel('Required Euler angles of the body [deg]')
+xlabel('Time [s]')
+set(gcf, 'Position', [100, 100, 400, 400])
